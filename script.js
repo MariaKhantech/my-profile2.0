f9f11a3e9fbe182cdd2f376f2eba8cc2946a9fba
mmkhan <mariatechfuture@gmail.com>
2020-07-18 21:08:30 -0400

    working on form and profile portion of landing page

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 let scene = $('#scene').get(0);
 let parallaxInstance = new Parallax(scene);
+
+(function($) {
+	$('.btn').click(function() {
+		if ($(this).hasClass('.btn__liked')) {
+			$(this).removeClass('btn__liked');
+		} else {
+			$(this).addClass('btn__liked');
+		}
+	});
+})(jQuery);
